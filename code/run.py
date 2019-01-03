f20070ac9ede20a63fc7b322037ddd339ab5fb19
Alf <alfonso.barragan@alu.uclm.es>
2019-01-03 13:56:32 +0100

    Rediseño para aumentar la funcionalidad del proyecto

--- code/run.py
+++ code/run.py
@@ -5,5 +5,53 @@
 # Marcos López Sobrino
 # Roberto Plaza Romero
 
-from myapp import app
-app.run(host="0.0.0.0", port=8080)
+from flask import Flask, request, redirect, url_for, flash, render_template, make_response, jsonify
+from flask_oauthlib.client import OAuth
+
+import requests
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+oauth = OAuth()
+
+# Pagina principal
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/operations', methods=['GET'])
+def show_operations():
+
+    return render_template('tweets_adm.html')
+
+# Recogida de tweets
+
+@app.route('/collect', methods=['POST'])
+def collectTweets():
+    pass
+
+@app.route('/no_collect', methods=['POST'])
+def stopCollectTweets():
+    pass
+
+# Operaciones
+@app.route('/date_filter', methods=['POST'])
+def filterByDateTweets():
+    listTweets = []
+    pass
+
+@app.route('/list_word', methods=['POST'])
+def listByWordTweets():
+    pass
+
+@app.route('/show_likes', methods=['POST'])
+def showByZLikesTweets():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
+
+
